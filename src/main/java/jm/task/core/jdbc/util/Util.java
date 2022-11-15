870e3c3bb80b98ff8c646f43cee98c3d2a5ee8ac
Vladislav <100284407+plomb1r@users.noreply.github.com>
2022-11-15 22:05:08 +0300

    Delete JDBC functionality

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -9,31 +9,11 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.Properties;
 
 public class Util {
-    public static final String DB_USER = "root";
-    public static final String USER_PASSWORD = "1234";
-    public static final String DB_URL = "jdbc:mysql://localhost:3306/users1";
-    public static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
-    public static Connection connection = null;
-    private static SessionFactory concreteSessionFactory;
 
-    /**
-     * JDBC connection
-     */
-    public static Connection getConnection() {
-        try {
-            connection = DriverManager.getConnection(DB_URL, DB_USER, USER_PASSWORD);
-            Class.forName(DB_DRIVER);
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-        }
-        return connection;
-    }
+    private static SessionFactory concreteSessionFactory;
 
     /**
      * Hibernate configuration
